7f4789a933760e3f6a9767977011420c90834fc4
lou1s vuong <lou1svuong.dev@gmail.com>
2024-12-14 02:26:51 +0700

    fix: correct import order in layout.tsx for consistent styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import "./globals.css";
+
 import Providers from "@/providers";
 import { Toaster } from "@/components/ui/toaster";
 import TopLoader from "@/components/top-loader";
