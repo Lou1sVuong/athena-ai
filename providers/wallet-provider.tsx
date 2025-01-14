5ea5354985cb4be19522600cd881db604204ab3b
lou1s vuong <lou1svuong.dev@gmail.com>
2025-01-14 20:48:30 +0700

    fix: remove kairos chain and update transport configuration in wallet provider
    - Removed the kairos chain from the wallet provider configuration.
    - Updated the transport settings to use the mainnet base URL instead of the kairos testnet URL.
    - Cleaned up imports to reflect the changes in the chains used.

--- providers/wallet-provider.tsx
+++ providers/wallet-provider.tsx
@@ -8,7 +8,7 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { trustWallet, ledgerWallet } from "@rainbow-me/rainbowkit/wallets";
-import { baseSepolia, kairos, base } from "wagmi/chains";
+import { baseSepolia, base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, http } from "wagmi";
 
@@ -24,10 +24,10 @@ export const config = getDefaultConfig({
       wallets: [trustWallet, ledgerWallet],
     },
   ],
-  chains: [baseSepolia, base, kairos],
+  chains: [baseSepolia, base],
   transports: {
     [baseSepolia.id]: http("https://sepolia.base.org"),
-    [kairos.id]: http("https://rpc.ankr.com/klaytn_testnet"),
+    [base.id]: http("https://mainnet.base.org"),
   },
   ssr: true,
 });
